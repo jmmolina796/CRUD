72e8ba940279b994beee04896c15c33f833a991d
Gamaliel Rodríguez Roque <gamalielr68@gmail.com>
2018-03-17 15:38:12 -0600

    Semesters model created

---
+++ database/semesters.sql
@@ -0,0 +1,10 @@
+USE crudDB;
+
+CREATE TABLE semesters (
+	id_semester INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20)
+);
+
+INSERT INTO semesters VALUES (1, 'First');
+INSERT INTO semesters VALUES (2, 'Second');
+INSERT INTO semesters VALUES (3, 'Thirt');
--- database/subjects.sql
+++ database/subjects.sql
@@ -1,13 +1,15 @@
-CREATE DATABASE crudDB;
-
 USE crudDB;
 
 CREATE TABLE subjects (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30),
-  description TEXT
+  id_categories INT NOT NULL,
+  id_semester INT NOT NULL,
+  FOREIGN KEY (id_categories) REFERENCES categories (id_categories),
+  FOREIGN KEY (id_semester) REFERENCES semester (id_semester),
+  credits INT
 );
 
-INSERT INTO subjects VALUES (1, 'History', 'History Helps Us Understand Change and How the Society We Liv in Came.');
-INSERT INTO subjects VALUES (2, 'Chemistry', 'The branch of science that deals with the identification of the substances of which matter is composed.');
-INSERT INTO subjects VALUES (3, 'Music', 'Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion.');
+INSERT INTO subjects VALUES (1, 'History', 2, 1, 4);
+INSERT INTO subjects VALUES (2, 'Chemistry', 1, 2, 5);
+INSERT INTO subjects VALUES (3, 'Music', 3, 1, 3);
